0f7bcf21105de2cf9baff3ef3d0c1b79e05d08ca
Noah Söng <noah.song@elev.ga.ntig.se>
2024-10-09 11:16:05 +0200

    Gjort Tamagotchi klassen och gjort några händelser på main klassen

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tamagotchi!");
+
+        Scanner keyboard = new Scanner(System.in);
+        Tamagotchi Tamagotchi = new Tamagotchi();
+
+        System.out.println("Choose a name for your Tamagotchi:");
+        Tamagotchi.name=keyboard.nextLine();
+        System.out.println("Nice name!");
+
+        while(Tamagotchi.getAlive() == true){
+        Tamagotchi.printStats();
+            System.out.println("What do you want to do?");
+            System.out.println("1. Teach " + Tamagotchi.name + " a new word");
+            System.out.println("2. Talk to " + Tamagotchi.name);
+            System.out.println("3. Feed " + Tamagotchi.name);
+            System.out.println("4. Do nothing");
+
+            String doWhat = keyboard.nextLine();
+
+            if(doWhat.equals("1")){
+                System.out.println("What word do you want to teach your Tamagotchi?");
+                String word = keyboard.nextLine();
+                Tamagotchi.teach(word);
+            }
+
+            else if (doWhat.equals("2"));{
+                Tamagotchi.speak();
+            }
+
+            else if (doWhat.equals("3"));{
+
+            }
+        }
+    }
+
+
+
+}
---
+++ src/Tamagotchi.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Tamagotchi {
+private int hunger;
+private int boredom;
+private ArrayList words = new ArrayList();
+private boolean isAlive;
+private Random generator = new Random();
+public String name;
+
+    public Tamagotchi() {
+        isAlive = true;
+    }
+
+public void feed(){
+    System.out.println(name + " eats and becomes less hungry");
+    hunger -= 2;
+    if (hunger < 0) {
+        hunger = 0;
+    }
+}
+
+public  void speak(){
+    int wordNum = generator.nextInt(words.size());
+    System.out.println(name + " says: " + words.get(wordNum));
+    reduceBoredom();
+}
+
+public void teach(String word){
+    System.out.println(name+ " learns " + word);
+    words.add(word);
+    reduceBoredom();
+}
+
+
+public void tick(){
+    hunger++;
+    boredom++;
+    if (hunger > 10 || boredom > 10) {
+        isAlive = false;
+    }
+}
+
+public void printStats(){
+    System.out.println("Name: " + name + " || Hunger: " + hunger + " || Boredom: " + boredom + " || Vocabulary: " + words.size() + "words");
+}
+
+public boolean getAlive(){
+    return isAlive;
+}
+
+private void reduceBoredom(){
+    System.out.println(name + " is now less bored!");
+    boredom -= 2;
+    if (boredom < 0) {
+        boredom = 0;
+    }
+}
+}
